fa1e3ef86e3d9359d2da19e98c12edca90da60de
margaritaradeva <u12mr20@abdn.ac.uk>
2024-02-26 17:14:27 +0000

    Update settings for Heroku and add postgre plan

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import environ
 import dj_database_url
+import os
+
 
 env = environ.Env()
 environ.Env.read_env()
@@ -103,9 +105,8 @@ DATABASES = {
     #     'HOST': env("DB_HOST"),
     #     'PORT': env("DB_PORT"),
     # }
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600, ssl_require=True)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
