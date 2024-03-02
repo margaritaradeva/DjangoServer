6e63b7764aa1b987a8f618e44afef8a1faa303c6
margaritaradeva <u12mr20@abdn.ac.uk>
2024-03-02 22:47:29 +0000

    added dataclasses for more convenience

--- application/serializers.py
+++ application/serializers.py
@@ -1,6 +1,6 @@
 from application.models import CustomUser
 from rest_framework import serializers
-
+import services
 
 class CustomUserUserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True) # Shouldn't be able to change an id of a user
@@ -9,3 +9,6 @@ class CustomUserUserSerializer(serializers.Serializer):
     email = serializers.CharField()
     password = serializers.CharField(write_only=True) # Don't ever want to return a password in an API response 
         
+    def to_internal_value(self, data):
+        data = super().to_internal_value(data)
+        return services.CustomUserDataclass(**data) 
---
+++ application/services.py
@@ -0,0 +1,38 @@
+import dataclasses
+from typing import TYPE_CHECKING
+from . import models 
+
+if TYPE_CHECKING:
+    from .models import CustomUser
+
+@dataclasses.dataclass
+class CustomUserDataclass:
+    first_name: str
+    last_name: str
+    email: str
+    password: str = None
+    id: int = None
+
+    @classmethod
+    def from_instance(cls, dataclass_user: "CustomUser") -> "CustomUserDataclass":
+       # Now we can do sth like data.first_name rather than
+        # data["first_name"]
+        return cls(
+            first_name=dataclass_user.first_name,
+            last_name=dataclass_user.last_name,
+            email=dataclass_user.email,
+            id=dataclass_user.id
+        )
+    
+    def create_user(dataclass_user: "CustomUserDataclass") -> "CustomUserDataclass":
+        instance = models.CustomUser(
+            first_name=dataclass_user.first_name,
+            last_name=dataclass_user.last_name,
+            email=dataclass_user.email
+        )
+        if dataclass_user.password is not None:
+            instance.set_password(dataclass_user.password)
+
+        instance.save()
+
+        return CustomUserDataclass.from_instance(instance)
