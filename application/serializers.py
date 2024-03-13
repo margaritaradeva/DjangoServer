faa256723c3e9b9e2905dc119df857001d4c5f0e
margaritaradeva <u12mr20@abdn.ac.uk>
2024-03-13 20:43:25 +0000

    changed serialziers back to serialziers.Serialzier

--- application/serializers.py
+++ application/serializers.py
@@ -4,18 +4,13 @@ from rest_framework import serializers
 from .models import validate_password
 import re
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class CustomUserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True) # Shouldn't be able to change an id of a user
     first_name = serializers.CharField()
     last_name = serializers.CharField()
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True) # Don't ever want to return a password in an API response 
     total_brush_time = serializers.IntegerField()
-    
-    class Meta:
-        model=CustomUser
-        fields = {'id', 'first_name', 'last_name', 'password', 'total_brush_time'}
-        extra_kwargs = {'password': {'write_only': True}} 
     
     def create(self, data):
         user = CustomUser.objects.create(
