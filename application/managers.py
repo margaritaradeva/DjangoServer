47de65eb13422acfc50e1025c0f431bed8cd5cda
margaritaradeva <u12mr20@abdn.ac.uk>
2024-04-23 22:09:16 +0100

    ginal refactoring of managers.py

--- application/managers.py
+++ application/managers.py
@@ -3,27 +3,51 @@ This custom manager provides helper methods to create regular and superusers, as
 the default user model expects a username, which I am not using. The reason to do so is 
 because I want email verification and a username becomes obsolete
 """
+
 from django.contrib.auth.base_user import (
     BaseUserManager,
 )  # Starting point from which I inherit for the manager
 
+
 class CustomUserManager(BaseUserManager):
     """
     This manager is a subclass of BaseUserManager and overrides two methods-create user and superuser. This
-    is a necessary step as I need to handle the case where the email is the unique identifier.
+    is a necessary step as I need to handle the case where the email is the unique authentication identifier.
     """
 
-    def create_user(self, first_name: str, last_name: str, email: str, password: str, total_brush_time=0,
-                     current_level=1, current_level_xp=0, current_level_max_xp=120, image_id=1,
-                      current_streak = 0, max_streak = 0, total_brushes = 0, is_staff = False, is_superuser = False):
+    def create_user(
+        self,
+        first_name: str,
+        last_name: str,
+        email: str,
+        password: str,
+        total_brush_time=0,
+        current_level=1,
+        current_level_xp=0,
+        current_level_max_xp=120,
+        image_id=1,
+        current_streak=0,
+        max_streak=0,
+        total_brushes=0,
+        is_staff=False,
+        is_superuser=False,
+    ):
         """
         Create and save a user with the provided email and password
 
-        Args:
+        Parameters:
             first_name (str): user's first name]
             last_name (str): user's last name
             email (str): The email address of the user.
             password (str): The password of the user.
+            total_brush_time (int): The total number of seconds a user has brushed their teeth for
+            current_level (int): A user's current level
+            current_level_xp (int): Current level experience points acquired by a user
+            current_level_max_xp (int): Current level's maximum experience points possible
+            image_id (int): Used to indicate which customised character should be rendered
+            current_streak (int): User's current number of consecutive days that they have brushed their teeth
+            max_streak (int): User's maximum number of consecutive days they have brushed their teeth
+            total_brushes (int): Total number of brushes completed by a user
             is_staff (Boolean, optional): can the user access the admin site
             is_superuser: is the user a superuser with all privilleges
 
@@ -50,6 +74,7 @@ class CustomUserManager(BaseUserManager):
         user.last_name = last_name
         # Password hashing
         user.set_password(password)
+        # Set the values to the associated user fields and set to the deault ones if not provided
         user.total_brush_time = total_brush_time
         user.current_level = current_level
         user.current_level_xp = current_level_xp
@@ -71,21 +96,22 @@ class CustomUserManager(BaseUserManager):
         last_name: str,
         email: str,
         password: str,
-       ):
+    ):
         """
-        Create and save a SUPERuser with the provided email and password. Superusers have all permissions
+        Create and save a superuser with the provided email and password. Superusers have all permissions
         by default and can access the admin site (at ..../admin/)
 
-        Args:
+        Parameters:
             first_name (str): Superusers first name
             last_name (str): Superusers last name
             email (str): The email address of the superuser.
             password (str): The password of the superuser.
 
         Returns:
-            CustomUser: The created superuser instance.
+            CustomUser: The created superuser instance with full persmissions.
 
         """
-        user = self.create_user(first_name, last_name,email, password,is_staff=True,is_superuser=True)
+        user = self.create_user(
+            first_name, last_name, email, password, is_staff=True, is_superuser=True
+        )
         user.save()
-    
