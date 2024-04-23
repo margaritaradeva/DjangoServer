3f5c52d90ca5b82582b0d1378d90d4040f5bc1a6
margaritaradeva <u12mr20@abdn.ac.uk>
2024-04-23 22:16:51 +0100

    final refactoring of apps.py

--- application/apps.py
+++ application/apps.py
@@ -2,5 +2,13 @@ from django.apps import AppConfig
 
 
 class ApplicationConfig(AppConfig):
+    """ "
+    Configuration for the application app.
+
+    Attributes:
+    default_auto_field (str): Specifies the type of auto field to use as a primary key
+    nae (str): Defines the full Python path to the application
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "application"
